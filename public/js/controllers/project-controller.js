a36181aac3c79e49bbe520f37ce4530cd5eb4113
Inspix <inspix89@gmail.com>
2016-05-04 08:43:53 +0300

    Styles

--- public/js/controllers/project-controller.js
+++ public/js/controllers/project-controller.js
@@ -11,21 +11,25 @@ angular.module('issueTracker.project',[])
         function($scope,$routeParams){
             $scope.title = 'Title';
             $scope.projectId = $routeParams.id;
+            $scope.description = "Project description goes here"
             $scope.author = 'Author';
             $scope.content = 'Content here';
             $scope.issues = [
                 {
                     title:'Title 1',
+                    issueId: "#123123",
                     description: 'Description 1',
                     link: '/testLink1'
                 },
                 {
                     title:'Title 2',
+                    issueId: "#321334",
                     description: 'Description 2',
                     link: '/testLink2'
                 },
                 {
                     title:'Title 3',
+                    issueId: "#532452",
                     description: 'Description 3',
                     link: '/testLink3'
                 },
